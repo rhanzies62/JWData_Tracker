0fdf84cb1276d216ad273e43d8bab2e0b94ea20b
System Administrator <root@Franciss-MacBook-Pro.local>
2022-06-26 16:42:36 +0800

    congregation user service fixed

--- JWDataTracker.Application/CongregationUser/CongregationUserDto.cs
+++ JWDataTracker.Application/CongregationUser/CongregationUserDto.cs
@@ -5,15 +5,16 @@ namespace JWDataTracker.Application.CongregationUser
     {
         public CongregationUserDto()
         {
-            public long CongregationUserId { get; set; }
-            public long PublisherId { get; set; }
-            public string Username { get; set; }
-            public string Salt { get; set; }
-            public string Password { get; set; }
-            public long RoleId { get; set; }
-            public string CreatedDate { get; set; }
-            public long IsPasswordReset { get; set; }
-            public string Email { get; set; }
+
         }
+        public long CongregationUserId { get; set; }
+        public long PublisherId { get; set; }
+        public string Username { get; set; }
+        public string Salt { get; set; }
+        public string Password { get; set; }
+        public long RoleId { get; set; }
+        public string CreatedDate { get; set; }
+        public long IsPasswordReset { get; set; }
+        public string Email { get; set; }
     }
 }
