b2108cb48aa4e0a9c70a316b903ee2097fe35205
franciscebu <tblfrancis.cebu@gmail.com>
2022-07-04 12:47:20 +0800

    updated unit test

--- JWDataTracker.Test/UnitTest1.cs
+++ JWDataTracker.Test/UnitTest1.cs
@@ -6,19 +6,28 @@ namespace JWDataTracker.Test
 {
     public class Tests
     {
+        //For Retrieving
+        private DataTrackerContext dbContext;
+        private IGenericRepository<entity.Publisher> publisherRepo;
+        private IGenericRepository<entity.CongregationUser> congregationRepo;
+
+        //For Adding
         private readonly Mock<IGenericRepository<entity.CongregationUser>> congregationRepoMoq = new Mock<IGenericRepository<entity.CongregationUser>>();
         private readonly Mock<IGenericRepository<entity.Publisher>> publisherRepoMoq = new Mock<IGenericRepository<entity.Publisher>>();
         private readonly Mock<IUnitOfWork> unitOfWork = new Mock<IUnitOfWork>();
         [SetUp]
         public void Setup()
         {
+            dbContext = new DataTrackerContext();
+            publisherRepo = new GenericRepository<entity.Publisher>(dbContext);
+            congregationRepo = new GenericRepository<entity.CongregationUser>(dbContext);
         }
 
         [Test]
         public void Login_Success()
         {
-            unitOfWork.Setup(m => m.PublisherRepository).Returns(new GenericRepository<entity.Publisher>(new DataTrackerContext()));
-            unitOfWork.Setup(m => m.CongregationUserRepository).Returns(new GenericRepository<entity.CongregationUser>(new DataTrackerContext()));
+            unitOfWork.Setup(m => m.PublisherRepository).Returns(publisherRepo);
+            unitOfWork.Setup(m => m.CongregationUserRepository).Returns(congregationRepo);
             var service = new CongregationUserService(unitOfWork.Object);
             var result = service.GetUserByUsernameAndPassword(new CongregationUserDto {
                 Username = "rdelacruz",
@@ -26,6 +35,12 @@ namespace JWDataTracker.Test
             });
 
             Assert.IsTrue(result.IsSuccess);
+        }
+
+        [Test]
+        public void Login_Failed()
+        {
+
         }
     }
 }
