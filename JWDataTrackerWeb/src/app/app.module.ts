a4d958713d7060106e5c1dc90ac4e600abf89f97
franciscebu <tblfrancis.cebu@gmail.com>
2022-07-07 22:07:44 +0800

    Added login, forgot password and dashboard pages, including navigation header

--- JWDataTrackerWeb/src/app/app.module.ts
+++ JWDataTrackerWeb/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavMenuComponent } from './shared/components/nav-menu/nav-menu.component';
+import { MainContentComponent } from './pages/main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    DashboardComponent,
+    NavMenuComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent, pathMatch: 'full' },
+      { path: 'forgotpassword', component: ForgotPasswordComponent, pathMatch: 'full' },
+      {
+        path: '', component: MainContentComponent, children: [
+          { path: 'dashboard', component: DashboardComponent} 
+        ]
+      }
+    ], { scrollPositionRestoration: 'enabled' })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
